44c867d123f12b5e688b70be2dcd8f01d764dd80
Zapmal <mzxgd16@gmail.com>
2021-08-17 18:03:54 -0400

    chore(config): absolute paths setup
    Allow the usage of `@<folder>/<file>` by using `tsconfig.json`,
    `jsconfig.json` is for intellisense.

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
 
+import { styled } from '../stitches.config';
+
 import { ChakraProvider } from '@chakra-ui/react';
+
+const Button = styled('button', {
+  margin: '$3',
+  padding: '$4',
+  color: 'white',
+  variants: {
+    type: {
+      success: {
+        backgroundColor: '$successSolid',
+      },
+      danger: {
+        backgroundColor: '$dangerSolid',
+      },
+      warning: {
+        backgroundColor: '$warningSolid',
+      },
+      base: {
+        backgroundColor: '$primaryBg',
+      },
+      accent: {
+        backgroundColor: '$accentSolid',
+      },
+      info: {
+        backgroundColor: '$infoSolid'
+      }
+    }
+  }
+});
 
 function App() {
   return (
     <ChakraProvider>
-      <p>hello</p>
+      <Button type='success'>Success</Button>
+      <Button type='danger'>Danger</Button>
+      <Button type='warning'>Warning</Button>
+      <Button type='base'>Base</Button>
+      <Button type='accent'>Accent</Button>
+      <Button type='info'>Info</Button>
     </ChakraProvider>
   );
 }
--- web/vite.config.js
+++ web/vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite';
+import path from 'path';
+
 import reactRefresh from '@vitejs/plugin-react-refresh';
 
 export default defineConfig({
   plugins: [reactRefresh()],
+  resolve: {
+    alias: {
+      '@example': path.resolve(__dirname, './src/example'),
+    }
+  }
 });

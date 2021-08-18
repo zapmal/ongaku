99c863d13c2bc3c2f592a8079cf554ab0bcc7a3e
Zapmal <mzxgd16@gmail.com>
2021-08-18 17:31:53 -0400

    chore: update .gitignore and fix lint issue

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
 import path from 'path';
 
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [reactRefresh()],
   resolve: {
     alias: {
       '@example': path.resolve(__dirname, 'src/example'),
-    }
-  }
+    },
+  },
 });

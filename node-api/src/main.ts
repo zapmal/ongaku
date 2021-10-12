39fd29d192839ad351abbe73aea86d6f669cbd53
Zapmal <mzxgd16@gmail.com>
2021-10-12 17:58:43 -0400

    fix: update prisma service

--- node-api/src/main.ts
+++ node-api/src/main.ts
@@ -1,14 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
 import * as cookieParser from 'cookie-parser';
 import * as csurf from 'csurf';
 import * as helmet from 'helmet';
-import { cookieOptions } from './utils/cookie';
+
+import { AppModule } from '@/app.module';
+import { cookieOptions } from '@utils/cookie';
+import { PrismaService } from '@common/services';
 
 async function bootstrap() {
   const PORT = 4000 || process.env.PORT;
   const app = await NestFactory.create(AppModule);
+  const prismaService: PrismaService = app.get(PrismaService);
+  prismaService.enableShutdownHooks(app);
 
   app.setGlobalPrefix('api');
 

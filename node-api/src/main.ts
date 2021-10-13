9746def17c9e2c478b59a8649cdf00107d3ab0eb
Zapmal <mzxgd16@gmail.com>
2021-10-13 18:07:18 -0400

    refactor: move frontend_url to .env

--- node-api/src/main.ts
+++ node-api/src/main.ts
@@ -8,7 +8,7 @@ import { cookieOptions } from '@utils/cookie';
 import { PrismaService } from '@common/services';
 
 async function bootstrap() {
-  const PORT = 4000 || process.env.PORT;
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
   const prismaService: PrismaService = app.get(PrismaService);
   prismaService.enableShutdownHooks(app);
@@ -16,7 +16,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   app.use(helmet());
-  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
+  app.enableCors({ origin: process.env.FRONTEND_URL, credentials: true });
   app.use(cookieParser());
   app.use(csurf({ cookie: { ...cookieOptions } }));
 

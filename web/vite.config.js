27f726d9988fadb1b563e288580b805304098f9a
Zapmal <mzxgd16@gmail.com>
2021-12-15 14:31:42 -0400

    fix: modes for deployment and proxy

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,23 +1,27 @@
 import path from 'path';
 
 import reactRefresh from '@vitejs/plugin-react-refresh';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  plugins: [reactRefresh()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
-        changeOrigin: true,
-        secure: false,
-        // ws: true,
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return {
+    plugins: [reactRefresh()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
       },
     },
-  },
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.VITE_NODE_API_URL,
+          changeOrigin: true,
+          secure: false,
+          // ws: true,
+        },
+      },
+    },
+  };
 });
